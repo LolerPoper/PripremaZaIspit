a4a12202b2f57058f475f82e3e9660512ecb7b06
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2022-12-04 19:12:23 +0100

    Add files via upload

---
+++ PripremaIspit-Trokut/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PripremaIspit_Trokut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            programStart:
+            Console.WriteLine("Upisite stranicu a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Upisite stranicu b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Upisite stranicu c: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (a == 0 || b == 0 || c == 0)
+            {
+                Console.WriteLine("Stranica nema smisleni broj, pokusaj ponovo.");
+                goto programStart;
+            }
+            else
+            {
+                if (Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2)) Console.WriteLine("Dane stranice su stranice pravokutnog trokuta.");
+                else Console.WriteLine("Dane stranice nisu stranice pravokutnog trokuta.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
